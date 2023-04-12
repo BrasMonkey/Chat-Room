8bcfd1c9e54569980ddce221392a0bd8620dde00
BrassMonkey <tatiana.zubrienko@gmail.com>
2023-04-12 17:12:15 +0200

    Visual part ajusted

--- Client.py
+++ Client.py
@@ -39,30 +39,30 @@ msg_frame.grid()
 scroll_bar = tkinter.Scrollbar(msg_frame)
 scroll_bar.grid(row=0, column=1, sticky="ns")
 
-msg_zone = tkinter.Listbox(msg_frame, height = 15, width = 100, bg = "white", yscrollcommand = scroll_bar.set)
+msg_zone = tkinter.Listbox(msg_frame, height = 15, width = 100, bg = "#E4EFF0", yscrollcommand = scroll_bar.set,)
 msg_zone.grid(row=0,column=0,sticky="nsew")
 
-send_zone = tkinter.Frame(window, width = 100, height= 15)
-send_zone.grid()
+send_zone = tkinter.Frame(window, height= 15, bg= "#C5D7D9")
+send_zone.grid(sticky="nsew")
 
-label = tkinter.Label(send_zone, text="Message: ")
-label.grid(row=0)
+label = tkinter.Label(send_zone, text="MESSAGE: ",bg= "#C5D7D9")
+label.grid(row=0,padx=25)
 entry = tkinter.Entry(send_zone, textvariable = msg, width="50")
-entry.grid(row=0, column=1)
+entry.grid(row=0, column=1, padx=15, pady=5)
 
-send_button = tkinter.Button(send_zone, text = "Send", font = "Aerial", fg="black", command = send)
-send_button.grid(row=0, column=2)
+send_button = tkinter.Button(send_zone, text = "Send", font = "Aerial", fg="black", bg="#A5EB7C", relief = tkinter.RAISED, command = send)
+send_button.grid(row=0, column=2, padx=5, pady=5)
 
-quit_button = tkinter.Button(send_zone, text = "Quit", font = "Aerial", fg="white", bg="black", command = quit_chat)
-quit_button.grid(row=0, column=3)
+quit_button = tkinter.Button(send_zone, text = "Quit", font = "Aerial", fg="black", bg="#E8F0E4",relief = tkinter.RAISED, command = quit_chat)
+quit_button.grid(row=0, column=3, padx=5, pady=5)
 
-host = "localhost"
-port = 8080
+#host = "localhost"
+#port = 8080
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((host, port))
 
-receive_thrd = Thread(target=receive)
-receive_thrd.start()
+#receive_thrd = Thread(target=receive)
+#receive_thrd.start()
 
 tkinter.mainloop()
